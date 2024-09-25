d612bd578e0710e02c0513afab751d669a895148
alexm <alexmarchgomez@gmail.com>
2024-09-25 12:28:43 +0200

    Añadiendo el archivo hola

---
+++ Hola.py
@@ -0,0 +1,6 @@
+mysterious_number = 3
+user_number = int(input("Adivina un numero del 1 al 10: "))
+if mysterious_number == user_number:
+    print("Bien hecho, lo has adivinado")
+else:
+    print ("No, tonto")
