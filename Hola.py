94728073d38ec68e6fd40e358a170308c96ecf9d
alexm <alexmarchgomez@gmail.com>
2024-09-25 12:29:35 +0200

    Hola=adivinar un numero

--- Hola.py
+++ Hola.py
@@ -3,4 +3,4 @@ user_number = int(input("Adivina un numero del 1 al 10: "))
 if mysterious_number == user_number:
     print("Bien hecho, lo has adivinado")
 else:
-    print ("No, tonto")
+    print ("No, tontainas")
